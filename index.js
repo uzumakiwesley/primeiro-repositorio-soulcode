a1b8af365a072d351314f06569f093528d313c7e
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-09-16 16:52:51 -0300

    operadores de incremmento e decremento

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * -->Tipos primitivos
+ * -->Variáveis Javascript
+ * -->Operadores matemáticos do Javascript
+ */
+
+// serve para escrever mensagens no console
+
+// string --> textos
+// number --> números
+// boolean --> verdadeiro/falso
+
+console.log("Hello world")
+console.log(5)
+console.log(43.76)
+console.log(true)
+console.log(false)
+
+//variáveis
+let mensagem ='SoulCode BC 25 JFS'   // declaração de variável
+
+console.log(mensagem)
+
+mensagem = 'Novo texto'
+
+console.log(mensagem) // novo texto
+
