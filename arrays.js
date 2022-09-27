fc8e5647e8615de01eb768642dedb25767f92c2a
uzumakiwesley <uzumakiwesley58@gmail.com>
2022-09-27 01:29:08 -0300

    commit_exercicio1-2-3_2609

--- arrays.js
+++ arrays.js
@@ -22,3 +22,17 @@ arr[6] = 'outro texto'
 console.log(arr[6]) // outro texto
 console.log(arr[8]) // undefeined
 arr[8] = 'agora a posição 8 existe dentro da arr'
+
+const lista =['João', 'Maria', 'Paulo', 'Vitória', 'Augusto', 'Larissa']
+console.log(lista)
+lista.push('Roberta') // adicionar um valor no final do array
+console.log(lista)
+lista.unshift('Gustavo') // adicionar um valor ao inicio do array
+console.log(lista)
+lista.pop() // remove o ultimo nvalor do array
+console.log(lista)
+lista.shift() // remove o primeiro valor da array
+console.log(lista)
+console.log(lista.concat('julio'))
+console.log(lista)
+console.log(lista.join('-> '))
